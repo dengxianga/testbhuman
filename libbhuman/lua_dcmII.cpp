4ccbf590d56b54886ecb746bfaba7d00a7810537
dengxianga <dengxianga@users.noreply.github.com>
2017-05-29 23:09:47 -0400

    set get but bugs

--- libbhuman/lua_dcmII.cpp
+++ libbhuman/lua_dcmII.cpp
@@ -36,6 +36,7 @@ int fd;
 //int memoryHandle = shm_open(LBH_MEM_NAME, O_CREAT | O_RDWR, S_IRUSR | S_IWUSR);
 LBHData * data;
 bool initialized = false;
+int writingActuators = -1;
 
 static int luaBH_getdummy (lua_State *L) {
   lua_pushnumber(L, 0);
@@ -57,83 +58,96 @@ static void lua_initialize () {
   initialized = true;
 }
 
-// static void lua_pushdouble_array(lua_State *L, double *v, int n) {
-//   lua_createtable(L, n, 0);
-//   for (int i = 0; i < n; i++) {
-//     lua_pushnumber(L, v[i]);
-//     lua_rawseti(L, -2, i+1);
-//   }
-// }
+static void lua_pushdouble_array(lua_State *L, double *v, int n) {
+  lua_createtable(L, n, 0);
+  for (int i = 0; i < n; i++) {
+    lua_pushnumber(L, v[i]);
+    lua_rawseti(L, -2, i+1);
+  }
+}
 
-// static void lua_pushvector(lua_State *L, std::vector<double> &v) {
-//   int n = v.size();
-//   lua_createtable(L, n, 0);
-//   for (int i = 0; i < n; i++) {
-//     lua_pushnumber(L, v[i]);
-//     lua_rawseti(L, -2, i+1);
-//   }
-// }
+static void lua_pushvector(lua_State *L, std::vector<double> &v) {
+  int n = v.size();
+  lua_createtable(L, n, 0);
+  for (int i = 0; i < n; i++) {
+    lua_pushnumber(L, v[i]);
+    lua_rawseti(L, -2, i+1);
+  }
+}
 
-// static std::vector<double> lua_checkvector(lua_State *L, int narg) {
-//   if (!lua_istable(L, narg))
-//     luaL_typerror(L, narg, "vector");
-//   int n = lua_objlen(L, narg);
-//   std::vector<double> v(n);
-//   for (int i = 0; i < n; i++) {
-//     lua_rawgeti(L, narg, i+1);
-//     v[i] = lua_tonumber(L, -1);
-//     lua_pop(L, 1);
-//   }
-//   return v;
-// }
+static void openActuators(float*& actuators)
+{
+  assert(writingActuators == -1);
+  writingActuators = 0;
+  if(writingActuators == data->newestActuators)
+    ++writingActuators;
+  if(writingActuators == data->readingActuators)
+    if(++writingActuators == data->newestActuators)
+      ++writingActuators;
+  assert(writingActuators != data->newestActuators);
+  assert(writingActuators != data->readingActuators);
+  actuators = data->actuators[writingActuators];
+}
 
-// static int set_actuator_mode(lua_State *L) {
-//   int mode = luaL_checkint(L, 1);
-//   pActuator->mode = mode;
-//   return 0;
-// }
+static void closeActuators()
+{
+  assert(writingActuators >= 0);
+  data->newestActuators = writingActuators;
+  writingActuators = -1;
+}
 
-// static int set_actuator_position(lua_State *L) {
-//   double x = luaL_checknumber(L, 1);
-//   int index = luaL_checkint(L, 2) - 1; // convert to zero-indexed
-//   pActuator->position[index] = x;
-//   return 0;
-// }
+static std::vector<double> lua_checkvector(lua_State *L, int narg) {
+  if (!lua_istable(L, narg))
+    luaL_typerror(L, narg, "vector");
+  int n = lua_objlen(L, narg);
+  std::vector<double> v(n);
+  for (int i = 0; i < n; i++) {
+    lua_rawgeti(L, narg, i+1);
+    v[i] = lua_tonumber(L, -1);
+    lua_pop(L, 1);
+  }
+  return v;
+}
 
-// static int set_actuator_hardness(lua_State *L) {
-//   double x = luaL_checknumber(L, 1);
-//   int index = luaL_checkint(L, 2) - 1; // convert to zero-indexed
-//   pActuator->hardness[index] = x;
-//   return 0;
-// }
+static int set_actuator_positions(lua_State *L) {
+  if (!initialized) {
+    lua_initialize();
+  }
+  std::vector<double> vs = lua_checkvector(L, 1);
+  std::vector<double> ids = lua_checkvector(L, 2);
 
-// static int set_actuator_command(lua_State *L) {
-//   double x = luaL_checknumber(L, 1);
-//   int index = luaL_checkint(L, 2) - 1; // convert to zero-indexed
-//   pActuator->command[index] = x;
-//   return 0;
-// }
 
-// static int set_actuator_velocity(lua_State *L) {
-//   double x = luaL_checknumber(L, 1);
-//   int index = luaL_checkint(L, 2) - 1; // convert to zero-indexed
-//   pActuator->velocity[index] = x;
-//   return 0;
-// }
+  float * actuators;
+  openActuators(actuators);
 
-// static int set_actuator_joint_imu_angle_x(lua_State *L) {
-//   double x = luaL_checknumber(L, 1);
-//   int index = luaL_checkint(L, 2) - 1; // convert to zero-indexed
-//   pActuator->jointImuAngleX[index] = x;
-//   return 0;
-// }
+  for (unsigned int i = 0; i < vs.size(); i++) {
+    actuators[(int)ids[i]-1] = vs[i];
+    std::cout<<" "<< vs[i]<<std::endl;
+  }
+  std::cout<<std::endl;
+  closeActuators();
 
-// static int set_actuator_joint_imu_angle_y(lua_State *L) {
-//   double x = luaL_checknumber(L, 1);
-//   int index = luaL_checkint(L, 2) - 1; // convert to zero-indexed
-//   pActuator->jointImuAngleY[index] = x;
-//   return 0;
-// }
+  return 0;
+}
+
+static int set_actuator_stiffneses(lua_State *L) {
+  if (!initialized) {
+    lua_initialize();
+  }
+  std::vector<double> vs = lua_checkvector(L, 1);
+  std::vector<double> ids = lua_checkvector(L, 2);
+
+  float * actuators;
+  openActuators(actuators);
+
+  for (unsigned int i = 0; i < vs.size(); i++) {
+    actuators[headYawStiffnessActuator + (int)ids[i] -1] = vs[i];
+  }
+
+  closeActuators();
+
+  return 0;
+}
 
 static int get_actuator_position(lua_State *L) {
   if (!initialized) {
@@ -149,7 +163,7 @@ static int get_actuator_hardness(lua_State *L) {
   if (!initialized) {
     lua_initialize();
   }
-  int index = luaL_checkint(L, 1) -1 + 25; // convert to zero-indexed, add 26 
+  int index = luaL_checkint(L, 1) -1 + 25; // convert to zero-indexed, add 26
   float * actuators = data->actuators[data->newestActuators];
   lua_pushnumber(L, (double) actuators[index]);
   return 1;
@@ -159,89 +173,26 @@ static int get_actuator_command(lua_State *L) {
   get_actuator_position(L);
 }
 
-// static int get_actuator_velocity(lua_State *L) {
-//   int index = luaL_checkint(L, 1) - 1; // convert to zero-indexed
-//   lua_pushnumber(L, pActuator->velocity[index]);
-//   return 1;
-// }
-
-// static int set_actuator_positions(lua_State *L) {
-//   std::vector<double> v = lua_checkvector(L, 1);
-//   int index = luaL_optint(L, 2, 1) - 1;
-//   for (unsigned int i = 0; i < v.size(); i++) {
-//     pActuator->position[index+i] = v[i];
-//   }
-//   return 0;
-// }
-
-// static int set_actuator_hardnesses(lua_State *L) {
-//   std::vector<double> v = lua_checkvector(L, 1);
-//   int index = luaL_optint(L, 2, 1) - 1;
-//   for (unsigned int i = 0; i < v.size(); i++) {
-//     pActuator->hardness[index+i] = v[i];
-//   }
-//   return 0;
-// }
-
-// static int get_actuator_hardnesses(lua_State *L) {
-//   lua_pushdouble_array(L, pActuator->hardness, nJoint);
-//   return 1;
-// }
 
 static int get_sensor_position(lua_State *L) {
   if (!initialized) {
     lua_initialize();
   }
-  int index = luaL_checkint(L, 1) -1; // convert to zero-indexed, add 26 
+  int index = luaL_checkint(L, 1) -1; // convert to zero-indexed, add 26
   float * sensors = data->sensors[data->newestSensors];
   lua_pushnumber(L, (double) sensors[index]);
   return 1;
 }
 
-// static int get_sensor_positions(lua_State *L) {
-//   lua_pushdouble_array(L, pSensor->position, nJoint);
-//   return 1;
-// }
-
-// static int get_imu_angle(lua_State *L) {
-//   lua_pushdouble_array(L, pSensor->imu, 2);
-//   return 1;
-// }
-
-// static int get_imu_acc(lua_State *L) {
-//   lua_pushdouble_array(L, pSensor->imu+2, 3);
-//   return 1;
-// }
-
-// static int get_imu_gyr(lua_State *L) {
-//   lua_pushdouble_array(L, pSensor->imu+5, 3);
-//   return 1;
-// }
 
 static const struct luaL_Reg bhlowcmd_lib [] = {
   {"getdummy", luaBH_getdummy},
-  // {"set_actuator_mode", set_actuator_mode},
-  // {"set_actuator_position", set_actuator_position},
-  // {"set_actuator_hardness", set_actuator_hardness},
-  // {"set_actuator_command", set_actuator_command},
-  // {"set_actuator_velocity", set_actuator_velocity},
-  // {"set_actuator_joint_imu_angle_x", set_actuator_joint_imu_angle_x},
-  // {"set_actuator_joint_imu_angle_y", set_actuator_joint_imu_angle_y},
+  {"set_actuator_stiffneses", set_actuator_stiffneses},
+  {"set_actuator_positions", set_actuator_positions},
   {"get_actuator_position", get_actuator_position},
   {"get_actuator_hardness", get_actuator_hardness},
   {"get_actuator_command", get_actuator_command},
-  // {"get_actuator_velocity", get_actuator_velocity},
-
-  // {"set_actuator_positions", set_actuator_positions},
-  // {"set_actuator_hardnesses", set_actuator_hardnesses},
-  // {"get_actuator_hardnesses", get_actuator_hardnesses},
-
   {"get_sensor_position", get_sensor_position},
-  // {"get_sensor_positions", get_sensor_positions},
-
-  // {"get_imu_angle", get_imu_angle},
-  // {"get_imu_acc", get_imu_acc},
-  // {"get_imu_gyr", get_imu_gyr},
   {NULL, NULL}
 };
 
