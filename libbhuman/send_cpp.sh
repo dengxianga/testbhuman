2766ecd39b715a0c023026566868e2235126a9c8
Fiona <fionala7@gmail.com>
2017-06-23 12:16:07 -0400

    added bash script to compile and send libbhuman.so, libdcmII.so, and test_dcmII

--- libbhuman/send_cpp.sh
+++
@@ -1,3 +0,0 @@
-scp -P 3333 dcmII.cpp nao@127.0.0.1:~/testbhuman/libbhuman
-scp -P 3333 dcmII.h nao@127.0.0.1:~/testbhuman/libbhuman
-scp -P 3333 test_dcmII/test_dcmII.cpp nao@127.0.0.1:~/testbhuman/libbhuman/test_dcmII
